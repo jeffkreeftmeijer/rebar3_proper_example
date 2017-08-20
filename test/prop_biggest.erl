eef47432f289fd8376b7667ad53f7e6e6afdd588
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-20 10:02:59 +0200

    Add prop_biggest:prop_biggest/0
    As per http://propertesting.com/book_stateless_properties.html#_writing_properties

--- test/prop_biggest.erl
+++ test/prop_biggest.erl
@@ -4,18 +4,21 @@
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
-prop_test() ->
-    ?FORALL(Type, term(),
+prop_biggest() ->
+    ?FORALL(List, non_empty(list(integer())),
         begin
-            boolean(Type)
+            biggest(List) =:= lists:last(lists:sort(List))
         end).
 
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
-boolean(_) -> true.
+biggest([Head|Tail]) ->
+  biggest(Tail, Head).
 
-%%%%%%%%%%%%%%%%%%
-%%% Generators %%%
-%%%%%%%%%%%%%%%%%%
-mytype() -> term().
+biggest([], Biggest) ->
+  Biggest;
+biggest([Head|Tail], Biggest) when Head >= Biggest ->
+  biggest(Tail, Head);
+biggest([Head|Tail], Biggest) when Head < Biggest ->
+  biggest(Tail, Biggest).
